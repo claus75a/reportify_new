c3596bca871a41a49c8795397aeb357d58db7994
starthomas610 <stmk93610@gmail.com>
2024-06-13 15:34:42 -0400

    commit basic source of milestone 2

--- public/userarea/importify/get_columnlist_from_csv.php
+++ public/userarea/importify/get_columnlist_from_csv.php
@@ -2,9 +2,10 @@
 require '../../vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Reader\IReader;
 if(isset($_FILES['f_csv'])) {
     $file = $_FILES['f_csv']['tmp_name'];
-    $spreadsheet = IOFactory::load($file);
+    $spreadsheet = IOFactory::load($file, IReader::READ_DATA_ONLY);
     $worksheet = $spreadsheet->getActiveSheet();
     $arr_info = $worksheet->toArray();
     if(count($arr_info) > 0) {
