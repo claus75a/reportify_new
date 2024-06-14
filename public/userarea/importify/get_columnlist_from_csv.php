10d7be1a04e12d764e5284a334cdd65c6bb94f52
claus75a <info@acscreativesolutions.com>
2024-06-14 08:29:05 +0200

    update files inside importify folder

--- public/userarea/importify/get_columnlist_from_csv.php
+++ public/userarea/importify/get_columnlist_from_csv.php
@@ -2,12 +2,13 @@
 require '../../vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
-if(isset($_FILES['f_csv'])) {
+
+if (isset($_FILES['f_csv'])) {
     $file = $_FILES['f_csv']['tmp_name'];
     $spreadsheet = IOFactory::load($file);
     $worksheet = $spreadsheet->getActiveSheet();
     $arr_info = $worksheet->toArray();
-    if(count($arr_info) > 0) {
+    if (count($arr_info) > 0) {
         die(json_encode($arr_info[0]));
     } else {
         die(json_encode(array()));
