37bc8c41c93acf470a4da3f0f3c50a50abd7782a
starthomas610 <stmk93610@gmail.com>
2024-06-17 04:18:57 -0400

    fixed merged err

--- public/userarea/importify/get_columnlist_from_csv.php
+++ public/userarea/importify/get_columnlist_from_csv.php
@@ -2,13 +2,9 @@
 require '../../vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
-<<<<<<< HEAD
+use PhpOffice\PhpSpreadsheet\Reader\IReader;
 
 if (isset($_FILES['f_csv'])) {
-=======
-use PhpOffice\PhpSpreadsheet\Reader\IReader;
-if(isset($_FILES['f_csv'])) {
->>>>>>> c3596bca871a41a49c8795397aeb357d58db7994
     $file = $_FILES['f_csv']['tmp_name'];
     $spreadsheet = IOFactory::load($file, IReader::READ_DATA_ONLY);
     $worksheet = $spreadsheet->getActiveSheet();
